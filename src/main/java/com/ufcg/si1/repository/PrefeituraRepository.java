9a0f7110533eeabe3f8d03453f3581e725c7701b
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-17 16:25:53 -0300

    Adicionando método nos repositórios para retornar todos os objetos de uma entidade em List

--- src/main/java/com/ufcg/si1/repository/PrefeituraRepository.java
+++ src/main/java/com/ufcg/si1/repository/PrefeituraRepository.java
@@ -3,10 +3,15 @@ package com.ufcg.si1.repository;
 import com.ufcg.si1.model.prefeitura.Prefeitura;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Repositório que define as operações da Prefeitura.
  *
  * @author Vélmer Oliveira.
  */
 public interface PrefeituraRepository extends CrudRepository<Prefeitura, Long> {
+
+    List<Prefeitura> findAll();
+
 }
--- src/main/java/com/ufcg/si1/repository/QueixaRepository.java
+++ src/main/java/com/ufcg/si1/repository/QueixaRepository.java
@@ -3,11 +3,15 @@ package com.ufcg.si1.repository;
 import com.ufcg.si1.model.vigilanciasanitaria.queixa.Queixa;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Repositório que define as operações da Queixa.
  *
  * @author Vélmer Oliveira.
  */
 public interface QueixaRepository extends CrudRepository<Queixa, Long> {
+
+    List<Queixa> findAll();
 
 }
