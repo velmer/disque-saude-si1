2ecdff6156f4b5cd83cfe19d9bc5a97aa34b33fc
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-18 08:43:38 -0300

    Criando classes para modularizar as constantes e caminhos utilizados no sistema

---
+++ src/main/java/com/ufcg/si1/util/Paths.java
@@ -0,0 +1,15 @@
+package com.ufcg.si1.util;
+
+/**
+ * Classe que declara os caminhos - utilizados na URL para acesso ao servidor - a fim de
+ * modularização e reutilização.
+ */
+public final class Paths {
+
+    /**
+     * Para evitar confusões, vamos deixar o construtor privado.
+     */
+    private Paths() {}
+
+
+}
