2f0258ba56ee0614884d07fe0be7d0e28a57456c
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-21 18:57:50 -0300

    Apagando arquivos não mais necessários do diretório bin

--- bin/PrefeituraApiController.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.si1.controller;
-
-/**
- * Controller responsável por receber e tratar requisições da Prefeitura.
- *
- * @author Vélmer Oliveira.
- */
-public class PrefeituraApiController {
-}
