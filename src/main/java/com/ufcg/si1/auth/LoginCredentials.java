c021a4e90216d5c5f086d8c2481cd64d30722a19
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-24 18:38:31 -0300

    Criando filtros para login e autenticação, além do service para autenticação

--- src/main/java/com/ufcg/si1/auth/LoginCredentials.java
+++ src/main/java/com/ufcg/si1/auth/LoginCredentials.java
@@ -2,22 +2,7 @@ package com.ufcg.si1.auth;
 
 public class LoginCredentials {
 
-    private String username;
-    private String password;
+    public String username;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String password;
 }
