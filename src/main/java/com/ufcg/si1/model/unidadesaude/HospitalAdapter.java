6abb6e9c8c42be3f64bed0944dfe581ee4eedf00
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-30 15:12:11 -0300

    Apenas para o heroku

--- src/main/java/com/ufcg/si1/model/unidadesaude/HospitalAdapter.java
+++ src/main/java/com/ufcg/si1/model/unidadesaude/HospitalAdapter.java
@@ -1,20 +1,15 @@
 package com.ufcg.si1.model.unidadesaude;
 
-import br.edu.ufcg.Hospital;
 import com.ufcg.si1.model.Endereco;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import java.util.Set;
 
 @Table(name = "TB_HOSPITAL")
 @Entity
 public class HospitalAdapter extends UnidadeSaude {
-
-    @Transient
-    private Hospital hospital;
 
     @Column
     private float numeroPacientesDia;
@@ -26,7 +21,6 @@ public class HospitalAdapter extends UnidadeSaude {
         super(descricao, endereco, especialidades, quantidadeMedicos);
 
         this.numeroPacientesDia = numeroPacientesDia;
-        this.hospital = new Hospital(descricao, quantidadeMedicos, numeroPacientesDia);
     }
 
     public float getTaxaPacientesPorDia() {
